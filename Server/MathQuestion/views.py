4132c95f9b41d13eb20795284d3838814ea2f9df
Long <731986844@qq.com>
2017-06-13 15:44:46 +0800

    在MathQuestion的APP中创建MathQuestion模型，并初步实现getMathQuestion的API

--- Server/MathQuestion/views.py
+++ Server/MathQuestion/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+# from django.utils import simplejson
+# import json
 
 # Create your views here.
+def index(request):
+    data = {"answer": "Hello, MathQuestion!"}
+    # return HttpResponse(simplejson.dumps(data, ensure_accii=False))
+    return JsonResponse(data)
+
+def getMathQuestion(request):
+    request.encoding = 'utf-8'
+    try:
+        difficulty = request.GET['difficulty']
+        questuonType = request.GET['questuonType']
+        data = {
+            "information": "success",
+            "difficulty": difficulty,
+            "questuonType": questuonType
+        }
+        return JsonResponse(data)
+    except Exception as e:
+        return JsonResponse({"information": "failure"})
+    
