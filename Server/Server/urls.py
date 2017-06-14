980657f462750dee7a31a9a9794d335395330b75
Long <731986844@qq.com>
2017-06-14 21:55:13 +0800

    注册OpenTrivia

--- Server/Server/urls.py
+++ Server/Server/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^MathQuestion/', include('MathQuestion.urls')),
+    url(r'^OpenTrivia/', include('OpenTrivia.urls'))
 ]
